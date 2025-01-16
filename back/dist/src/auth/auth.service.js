dc4c50350a235b9c6d856af820873d42bc71908a
francisco <fran_petru@hotmail.com>
2025-01-16 13:17:03 -0300

    probando

--- back/dist/src/auth/auth.service.js
+++ back/dist/src/auth/auth.service.js
@@ -28,11 +28,9 @@ let AuthService = class AuthService {
             sub: user.id || user.sub,
             role: user.role,
         };
-<<<<<<< HEAD
         console.log('Payload del token:', payload);
-=======
-        console.log("Payload del token:", payload);
->>>>>>> 34dfcdb61b0565aa9dd75fb4099c8e4b11fb5b10
+        return this.jwtService.sign(payload);
+        console.log('Payload del token:', payload);
         return this.jwtService.sign(payload);
     }
     async validatePassword(plainPassword, hashedPassword) {
